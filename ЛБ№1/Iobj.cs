a148157dd54d0ab5c43c9cb1b607808e2bd79979
EMRAZ1 <egokamix56@gmail.com>
2024-02-12 15:07:06 +0200

    Додав коментарі для інтерфейсу

--- ЛБ№1/Iobj.cs
+++ ЛБ№1/Iobj.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Інформація до кожної команди читається зверху вниз, тобто коментар зверху команди
 namespace ЛБ_1
 {
+    // Оголошення інтерфейсу Iobj
     internal interface Iobj
     {
+        // Метод для встановлення назви чи імені об'єкта (Name Value)
         public void Nv();
+        // Метод для встановлення атрибутів (Attributes)
         public void At();
+        // Метод для виведення інформації (Write Info)
         public void Wi();
     }
 }
