f6c8a80e0e4602c8fc7069b4c8fedd0866d55e1d
EMRAZ1 <egokamix56@gmail.com>
2024-02-12 15:16:43 +0200

    Додав крментарі до 2 класу

--- ЛБ№1/Class2.cs
+++ ЛБ№1/Class2.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ЛБ_1
 {
+    //Тут Класобдж2(Клас другого об'єкту) реалізує інтерфейс Іобдж, у відміності від першого класу тут можна написати будь який атрибут
     internal class ClassObj2 : Iobj
     {
         public string Name { get; set; }
@@ -13,6 +14,7 @@ namespace ЛБ_1
         
         public string Attr {  get; set; }
 
+        // Метод для надання імені об'єкту
         public void Nv()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -21,14 +23,14 @@ namespace ЛБ_1
             Console.WriteLine("Введіть колір для другого об'єкту");
             this.Color = Console.ReadLine();
         }
-
+        // Метод для встановлення атрибуту об'єкту, можна написати будь що
         public void At()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Введіть атрибут для другого об'єкту");
             this.Attr = Console.ReadLine();
         }
-
+        // Метод для виведення інформації про об'єкт, так само як і в 1 класі виводить кінцеву інформацію
         public void Wi()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
